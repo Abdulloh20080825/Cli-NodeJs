f225cdef742c1a38e06cc77426c05c589e79c962
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-07-11 18:32:16 +0500

    Save token

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import getArgs from "./helpers/args.js";
 import { printError, printHelp, printSuccess } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const startCli = () => {
   const args = getArgs(process.argv);
-  console.log(args);
 
   if (args.h) {
     printHelp();
@@ -13,6 +22,7 @@ const startCli = () => {
     // save
   }
   if (args.t) {
+    return saveToken(args.t);
     // save token
   }
 };
